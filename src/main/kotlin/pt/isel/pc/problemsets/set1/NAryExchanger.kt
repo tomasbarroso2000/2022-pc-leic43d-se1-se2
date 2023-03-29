d4a94c67a0a331107ff33a6300e2fc20a8d77f51
tomasbarroso2000 <tomasbarroso2000@gmail.com>
2023-03-29 20:49:20 +0100

    refactor NAryExchanger for Specific Signaling

--- src/main/kotlin/pt/isel/pc/problemsets/set1/NAryExchanger.kt
+++ src/main/kotlin/pt/isel/pc/problemsets/set1/NAryExchanger.kt
@@ -10,70 +10,81 @@ import kotlin.time.Duration
 
 const val MIN_GROUP_SIZE_VALUE = 2
 
-class NAryExchanger<T>(groupSize: Int) {
-
+class NAryExchanger<T>(private val groupSize: Int) {
     private val log = LoggerFactory.getLogger(NAryExchanger::class.java)
     private val mLock: Lock = ReentrantLock()
-    private val mCondition: Condition = mLock.newCondition()
-    private var availableGroupSize: Int = groupSize
-    private var requests = mutableListOf<Request>()
-    private var values: MutableList<T> = mutableListOf()
+    private val requests: MutableList<Request<T>> = mutableListOf()
 
-    private class Request(
+    private class Request<T>(
+        val condition: Condition,
+        val value: T,
+    ) {
         var isDone: Boolean = false
-    )
+        var values: MutableList<T> = mutableListOf()
+    }
 
     @Throws(InterruptedException::class)
     fun exchange(value: T, timeout: Duration): List<T>? {
+
         mLock.withLock {
 
-            if (availableGroupSize <= MIN_GROUP_SIZE_VALUE ||
+            if (groupSize <= MIN_GROUP_SIZE_VALUE ||
                 timeout.isZero
             ) return null
 
-            val request = Request()
-            requests.add(request)
-            values.add(value)
-
-            //fast-path
-            if (requests.size == availableGroupSize) {
-
-                repeat(availableGroupSize) { index ->
-                    requests[index].isDone = true
+            //fast path
+            if (requests.size == groupSize - 1) {
+                val values = computeValues(value)
+                (0 until groupSize - 1).forEach { index ->
+                    requests[index].values = values
+                    requests[index].condition.signal()
                 }
-
-                requests.clear()
-
-                mCondition.signalAll()
                 return values
             }
 
-            //waiting path
-            var remainingNanos: Long = timeout.inWholeNanoseconds
+            val myRequest: Request<T> = Request(mLock.newCondition(), value)
+            requests.add(myRequest)
+
+            var remainingTime: Long = timeout.inWholeNanoseconds
 
             while (true) {
                 try {
-                    remainingNanos = mCondition.awaitNanos(remainingNanos)
+                    remainingTime = myRequest.condition.awaitNanos(remainingTime)
 
-                    if(request.isDone) {
-                        return values
+                    if (myRequest.isDone) {
+                        requests.remove(myRequest)
+                        return myRequest.values
                     }
 
-                    values = values.subList(availableGroupSize, values.size - 1)
-                } catch(e: InterruptedException) {
-                    requests.remove(request)
-                    if(requests.isNotEmpty()) mCondition.signalAll()
+                    if (remainingTime <= 0) {
+                        // giving-up
+                        requests.remove(myRequest)
+                        return null
+                    }
+
+                } catch (e: InterruptedException) {
+                    if (myRequest.isDone) {
+                        Thread.currentThread().interrupt()
+                        requests.remove(myRequest)
+                        requests.firstOrNull()?.condition?.signal()
+                        return myRequest.values
+                    }
+
+                    requests.firstOrNull()?.condition?.signal()
                     throw e
                 }
-
-                if (remainingNanos <= 0) {
-                    // giving-up
-                    requests.remove(request)
-                    return null
-                }
-
             }
-
         }
+
     }
+
+    private fun computeValues(value: T): MutableList<T> =
+        (0 until groupSize - 1)
+            .map { index ->
+                requests[index].isDone = true
+                requests[index].value
+            }
+            .toMutableList()
+            .also { list -> list.add(value) }
+
 }
